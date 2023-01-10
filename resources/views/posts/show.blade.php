4a451f6f24fc3bd3482edd3bfdbe4d07c027317c
TakamuraKoga <kt0607@icloud.com>
2023-01-10 15:42:48 +0900

    ブログ投稿の編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     </head>
-    <body>
+    <body class="antialiased">
         <h1 class='tiitle'>
             {{ $post->title }}
         </h1>
@@ -18,6 +18,9 @@
                 <h3>本文</h3>
                 <p class='body'>{{ $post->body }}</p>
             </div>
+        </div>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
         </div>
         <div class="footer">
             <a href="/">戻る</a>
